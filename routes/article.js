28077175ed7533b20373c5c9349eb1aa0ca317ed
Sanjay R <sanjayprashanth1997@gmail.com>
2022-05-31 08:30:08 +0530

    changed the task details

--- routes/article.js
+++ routes/article.js
@@ -1,40 +1,53 @@
 import express from "express";
-import { deleteArticle, editAnArticle, getAllArticle, postAnArticle } from "../controllers/article.js";
+import {
+  deleteArticle,
+  editAnArticle,
+  getAllArticle,
+  postAnArticle,
+} from "../controllers/article.js";
 import { isSignedIn } from "../controllers/auth.js";
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.get("/article", isSignedIn,getAllArticle); 
+router.get("/article", isSignedIn, getAllArticle);
 
-router.post("/article",isSignedIn,postAnArticle); 
+router.post("/article", isSignedIn, postAnArticle);
 
-router.put("/article", isSignedIn,editAnArticle); 
+router.put("/article", isSignedIn, editAnArticle);
 
-router.delete("/article", isSignedIn, deleteArticle); 
+router.delete("/article", isSignedIn, deleteArticle);
 
-router.post("/str/task", isSignedIn, async(req, res)=>{
-    try {
-        const str = await req.body.string; 
-  
-        const sortedValue = str
-        .toLowerCase()
-        .split("")
-        .sort()
-        .filter(word => word !== " "); 
-    
-    const count = {}; 
-    const result = await sortedValue.join("")
-   const countedValue = await sortedValue.forEach(element => {
-    
-      count[element] = (count[element] || 0)+1; 
-      });
-  
-   
-return res.status(200).json({result, countedValue}); 
-    } catch (error) {
-       return res.status(500).json({error:"Internal server error"})
+router.post("/str/task", isSignedIn, async (req, res) => {
+  try {
+    const str = await req.body.string;
+    //removing the spaces
+    let strwithoutSpace = str.replace(/ /g, "");
+    //sorting the given array
+    let sortedArr = strwithoutSpace.toLowerCase().split("").sort();
+    //finding the space index
+    let spaceIndexArr = [];
+    let inputArr = input.split("");
+    for (let i = 0; i < inputArr.length; i++) {
+      if (inputArr[i] == " ") {
+        spaceIndexArr.push(i);
+      }
     }
-    
-})
+    //making the space index to spited array
+    for (let j = 0; j < spaceIndexArr.length; j++) {
+      sortedArr.splice(spaceIndexArr[j], 0, " ");
+    }
+    const resultArr = await sortedArr.join("");
+    //counting the number of aarays....
 
-export const articleRouter = router; 
+    const count = {};
+    const countedValue = await sortedArr.forEach((element) => {
+      count[element] = (count[element] || 0) + 1;
+    });
+
+    return res.status(200).json({ resultArr, countedValue });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+export const articleRouter = router;
